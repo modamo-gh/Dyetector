28461d15270489023614026825dbd8f58e38523d
Morgan Byrd <morganthemosaic@gmail.com>
2022-04-17 09:53:10 -0400

    Draw initial circles

--- sketch.js
+++ sketch.js
@@ -1,7 +1,21 @@
+const dots = [];
+
 function setup() {
-	createCanvas(400, 400);
+	createCanvas(displayWidth, displayHeight);
+
+	for (let i = 0; i < 3; i++) {
+		const x = (displayWidth * (i + 1)) / 4;
+		const y = displayHeight / 2;
+		const diameter = displayWidth * 0.2;
+
+		dots[i] = new Dot(x, y, diameter);
+	}
 }
 
 function draw() {
 	background(220);
+
+	for (const dot of dots) {
+		dot.draw();
+	}
 }

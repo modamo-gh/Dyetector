c4b1c86fa416950e4628e14a082ebf6063341de2
Morgan Byrd <morganthemosaic@gmail.com>
2022-04-17 20:11:40 -0400

    Set color for each circle

--- dots.js
+++ dots.js
@@ -3,9 +3,31 @@ class Dot {
 		this.x = x;
 		this.y = y;
 		this.diameter = diameter;
+		this.r = 0;
+		this.g = 0;
+		this.b = 0;
 	}
 
 	draw() {
+		fill(this.r, this.g, this.b);
 		circle(this.x, this.y, this.diameter);
+	}
+
+	getRadius() {
+		return this.diameter / 2;
+	}
+
+	getX() {
+		return this.x;
+	}
+
+	getY() {
+		return this.y;
+	}
+
+	setColor(r, g, b) {
+		this.r = r;
+		this.g = g;
+		this.b = b;
 	}
 }
--- sketch.js
+++ sketch.js
@@ -1,7 +1,15 @@
 const dots = [];
+let round = 1;
+const numberOfRounds = 10;
 
 function setup() {
 	createCanvas(displayWidth, displayHeight);
+
+	const r = random(255);
+	const g = random(255);
+	const b = random(255);
+
+	const roundVariance = 255 - (255 / numberOfRounds) * (round - 1);
 
 	for (let i = 0; i < 3; i++) {
 		const x = (displayWidth * (i + 1)) / 4;
@@ -10,6 +18,39 @@ function setup() {
 
 		dots[i] = new Dot(x, y, diameter);
 	}
+
+	let differentDot = Math.floor(random(dots.length));
+	let differentValue = Math.floor(random(3));
+
+	for (let i = 0; i < dots.length; i++) {
+		if (i === differentDot) {
+			switch (differentValue) {
+				case 0:
+					const redRange = getRange(r, roundVariance);
+					const randomR = random(redRange[0], redRange[1] + 1);
+					dots[i].setColor(randomR, g, b);
+					debugger;
+
+					break;
+				case 1:
+					const greenRange = getRange(g, roundVariance);
+					const randomG = random(greenRange[0], greenRange[1] + 1);
+					dots[i].setColor(r, randomG, b);
+					debugger;
+
+					break;
+				case 2:
+					const blueRange = getRange(b, roundVariance);
+					const randomB = random(blueRange[0], blueRange[1]);
+					dots[i].setColor(r, g, randomB);
+					debugger;
+
+					break;
+			}
+		} else {
+			dots[i].setColor(r, g, b);
+		}
+	}
 }
 
 function draw() {
@@ -17,5 +58,29 @@ function draw() {
 
 	for (const dot of dots) {
 		dot.draw();
+	}
+}
+
+function getRange(rgbValue, roundVariance) {
+	const range = [];
+
+	if (rgbValue - roundVariance < 0) {
+		range[0] = 0;
+	} else {
+		range[0] = rgbValue - roundVariance;
+	}
+
+	if (rgbValue + roundVariance > 255) {
+		range[1] = 255;
+	} else {
+		range[1] = rgbValue + roundVariance;
+	}
+
+	return range;
+}
+
+function mousePressed() {
+	for (let i = 0; i < dots.length; i++){
+		if(Math.sqrt((mouseX - dots[i].getX)() ** 2) + ((mouseY - dots[i].getY()) ** 2)) <= 
 	}
 }

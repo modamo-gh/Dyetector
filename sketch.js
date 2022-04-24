67843817a380800b161590032aa38582614c306c
Morgan Byrd <morganthemosaic@gmail.com>
2022-04-24 13:18:51 -0400

    Change drawCircles to colorCircles + comments

--- sketch.js
+++ sketch.js
@@ -14,7 +14,7 @@ function setup() {
 		dots[i] = new Dot(x, y, diameter);
 	}
 
-	drawCircles();
+	colorCircles();
 }
 
 function draw() {
@@ -31,6 +31,58 @@ function draw() {
 	textSize(64);
 	fill(0);
 	text(score, displayWidth * 0.95, displayWidth * 0.05);
+}
+
+function colorCircles() {
+	// Create random RGB color
+	const r = random(255);
+	const g = random(255);
+	const b = random(255);
+
+	/**
+	 * Each round technically gets more difficult
+	 * Because the range of possible values is reduced
+	 */
+	const roundVariance = 255 - (255 / numberOfRounds) * (round - 1);
+
+	// Each round one dot is a different shade that the other two
+	let differentDotIndex = Math.floor(random(dots.length));
+	dots[differentDotIndex].setIsDifferent(true);
+
+	// Represents which RGB value (so either r, g, or b) to adjust
+	let differentValue = Math.floor(random(3));
+
+	for (let i = 0; i < dots.length; i++) {
+		if (i === differentDotIndex) {
+			/**
+			 * For the outlier dot,
+			 * one of the RGB values is changed
+			 * depending on the value of differentValue
+			 */
+			switch (differentValue) {
+				case 0:
+					const redRange = getRange(r, roundVariance);
+					const randomR = random(redRange[0], redRange[1] + 1);
+					dots[i].setColor(randomR, g, b);
+
+					break;
+				case 1:
+					const greenRange = getRange(g, roundVariance);
+					const randomG = random(greenRange[0], greenRange[1] + 1);
+					dots[i].setColor(r, randomG, b);
+
+					break;
+				case 2:
+					const blueRange = getRange(b, roundVariance);
+					const randomB = random(blueRange[0], blueRange[1]);
+					dots[i].setColor(r, g, randomB);
+
+					break;
+			}
+		} else {
+			dots[i].setColor(r, g, b);
+		}
+	}
 }
 
 function getRange(rgbValue, roundVariance) {
@@ -66,51 +118,6 @@ function mousePressed() {
 		}
 
 		round++;
-		drawCircles();
-	}
-}
-
-function drawCircles() {
-	// Create random RGB color
-	const r = random(255);
-	const g = random(255);
-	const b = random(255);
-
-	/**
-	 * Each round technically gets more difficult
-	 * Because the range of possible values is reduced
-	 */
-	const roundVariance = 255 - (255 / numberOfRounds) * (round - 1);
-
-	let differentDotIndex = Math.floor(random(dots.length));
-	dots[differentDotIndex].setIsDifferent(true);
-
-	let differentValue = Math.floor(random(3));
-
-	for (let i = 0; i < dots.length; i++) {
-		if (i === differentDotIndex) {
-			switch (differentValue) {
-				case 0:
-					const redRange = getRange(r, roundVariance);
-					const randomR = random(redRange[0], redRange[1] + 1);
-					dots[i].setColor(randomR, g, b);
-
-					break;
-				case 1:
-					const greenRange = getRange(g, roundVariance);
-					const randomG = random(greenRange[0], greenRange[1] + 1);
-					dots[i].setColor(r, randomG, b);
-
-					break;
-				case 2:
-					const blueRange = getRange(b, roundVariance);
-					const randomB = random(blueRange[0], blueRange[1]);
-					dots[i].setColor(r, g, randomB);
-
-					break;
-			}
-		} else {
-			dots[i].setColor(r, g, b);
-		}
+		colorCircles();
 	}
 }
